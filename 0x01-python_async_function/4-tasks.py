dc9f1e369fbbf30571a70bea994c8553843b20e0
MinaSamirSaad <minasamir.nashid@gmail.com>
2024-07-13 01:50:35 +0300

    The code is nearly identical to wait_n except task_wait_random is being called

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -8,7 +8,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     '''Takes an integer n and an integer max_delay as arguments
     that waits for a random delay between 0 and max_delay
     using the function wait_random n times, then returns a list
